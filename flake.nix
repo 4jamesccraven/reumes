458217a8144748a414628ff188bdc214158ee435
4jamesccraven <4jamesccraven@gmail.com>
2025-06-06 07:08:11 -0400

    nix package

--- flake.nix
+++ flake.nix
@@ -7,13 +7,34 @@
   };
 
   outputs =
-    { flake-utils, nixpkgs, ... }:
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        packages.default = self.packages.${system}.reumes;
+        packages.reumes = pkgs.buildGoModule {
+          pname = "reumes";
+          version = "0.1.0";
+
+          src = ./.;
+
+          vendorHash = "sha256-ZU6t3B6U1JZwbA205monoknlrowa+5yCAG5U4gW1jO0=";
+
+          meta = {
+            homepage = "https://github.com/4jamesccraven/reumes";
+            license = pkgs.lib.licenses.gpl3;
+            mainProgram = "reumes";
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             go
